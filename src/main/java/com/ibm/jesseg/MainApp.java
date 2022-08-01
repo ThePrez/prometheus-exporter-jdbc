15d59f2005f1dbf7ebf9e54b4f678383f1cd9257
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2022-08-01 15:55:36 -0500

    code cleanup

--- src/main/java/com/ibm/jesseg/MainApp.java
+++ src/main/java/com/ibm/jesseg/MainApp.java
@@ -1,11 +1,9 @@
 package com.ibm.jesseg;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStreamWriter;
 import java.net.BindException;
 import java.net.InetSocketAddress;
 import java.util.Arrays;
@@ -22,13 +20,13 @@ import io.prometheus.client.exporter.HTTPServer;
 public class MainApp {
 
     public static void main(String[] _args) {
-       AppLogger logger = AppLogger.getSingleton(Boolean.getBoolean("promclient.verbose"));
+        AppLogger logger = AppLogger.getSingleton(Boolean.getBoolean("promclient.verbose"));
 
         List<String> args = Arrays.asList(_args);
-        if(args.contains("sc")  ){
+        if (args.contains("sc")) {
             try {
                 File yaml = writeResourcesToFile(logger, new File("prometheus.yml"), "prometheus.yml");
-                logger.println_success("Wrote Service Commander definition to file "+yaml.getAbsolutePath());
+                logger.println_success("Wrote Service Commander definition to file " + yaml.getAbsolutePath());
                 logger.println("");
                 logger.println("To register with Service Commander, first install Service Commander");
                 logger.println("version 1.5.0 or newer. ");
@@ -38,7 +36,7 @@ public class MainApp {
                 logger.println("");
                 logger.println("Or, run the following command to register this service for all users:");
                 logger.printfln("    ln -sf %s /QOpenSys/etc/sf/services/%s", yaml.getAbsolutePath(), yaml.getName());
-            }catch(Exception e) {
+            } catch (Exception e) {
                 logger.printfln_err("Error writing prometheus.yml to file: %s", e.getMessage());
                 System.exit(-3);
             }
@@ -58,13 +56,12 @@ public class MainApp {
             List<SQLMetricPopulator> populators = new LinkedList<SQLMetricPopulator>();
             for (SQLQuery query : config.getSQLQueries()) {
                 populators.add(new SQLMetricPopulator(logger, registry, config,
-                        new ConnectionManager(config), 
+                        new ConnectionManager(config),
                         query.getInterval(),
-                        query.isMultiRow(), 
-                        query.getSql(), 
-                        query.getShowHostname(), 
-                        query.getGaugePrefix()
-                        ));
+                        query.isMultiRow(),
+                        query.getSql(),
+                        query.getShowHostname(),
+                        query.getGaugePrefix()));
             }
             rootServer = com.sun.net.httpserver.HttpServer.create();
 
@@ -78,11 +75,11 @@ public class MainApp {
                 populator.gatherNow(12);
             }
             logger.println_success("Metrics collection verified.");
-            
+
             HTTPServer server = new HTTPServer.Builder()
                     .withHttpServer(rootServer)
                     .build();
-                    
+
             String successMessage = "\n\n\n";
             successMessage += "==============================================================\n";
             successMessage += "Successfully started Prometheus client on port " + port + "\n";
@@ -101,9 +98,9 @@ public class MainApp {
     private static File writeResourcesToFile(AppLogger _logger, File _dest, String _resource) throws IOException {
         InputStream in = MainApp.class.getResourceAsStream(_resource);
         if (null == in) {
-            throw new IOException("Could not find resource to create "+_dest.getName());
+            throw new IOException("Could not find resource to create " + _dest.getName());
         }
-        try(FileOutputStream out = new FileOutputStream(_dest, false)) {
+        try (FileOutputStream out = new FileOutputStream(_dest, false)) {
             byte[] buffer = new byte[1024];
             int len;
             while ((len = in.read(buffer)) > 0) {
@@ -112,6 +109,7 @@ public class MainApp {
         }
         return _dest;
     }
+
     private static File getConfigFile(AppLogger _logger) throws IOException {
         File ret = new File(System.getProperty("promclient.config", "config.json"));
         if (ret.exists()) {
